ac3ac801141c4568cc170751876d7525d3df0991
Zainab Karrar <zainabkarrarabdulhussen@gmail.com>
2026-08-12 11:24:58 +0300

    last edit

--- w.js
+++ w.js
@@ -1,24 +1,24 @@
-const apikey=e6d5ea86d6451ea656f4880e2708bece;
+const apikey="e6d5ea86d6451ea656f4880e2708bece";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 const input = document.getElementById('cityInput');
 const searchBtn = document.getElementById('searchBtn');
 
 async function wetherstate() {
     try{ 
-        const wth1=await fetch('https://api.openweathermap.org/data/2.5/weather?q=Basra&appid=YOUR_API_KEY&units=metric');
+        const wth1=await fetch(url);
         const wth2=await wth1.json()
-        if (wth2.name==input){
-            const temp=wth2.main.temp
-            const flk=wth2.main.feels_like
-            const prs=wth2.main.pressure
-            const humi=wth2.main.humidity
-            const state=wth2.weather.main
-            const clud=wth2.cloud.all
+        while(wth2.name==input){
+            const temp=wth2.main.temp;
+            const flk=wth2.main.feels_like;
+            const prs=wth2.main.pressure;
+            const humi=wth2.main.humidity;
+            const state=wth2.weather.main;
+            const clud=wth2.cloud.all;
         }catch(error){
             console.log(error)
         }
     }
-}    
+}  
 searchBtn.addEventListener('click',function(){
     let city=input 
     wetherstate();
